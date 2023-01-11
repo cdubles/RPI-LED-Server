7def7faf05f5bd47bda2a5849c125274ab10cd4a
Cody <33189090+cdubles@users.noreply.github.com>
2023-01-10 21:26:57 -0500

    fix syntax and variable name

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const https = require("https");
 const config = require("./config.json");
-const JWT = require("./jwt.js")
+const JWT = require("./jwt.js");
 var options = {
   key: fs.readFileSync("key.pem"),
   cert: fs.readFileSync("cert.pem"),
 };
 const express = require("express");
 const bodyParser = require("body-parser");
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 
 const app = express();
 const server = https.createServer(options, app);
@@ -22,66 +22,68 @@ app.use(bodyParser.json());
 app.post("/signIn", function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
-  console.log(req.body)
+  console.log(req.body);
   if (config.username == username && config.password == password) {
     console.log("signed in");
     //make and send jwt
-    var token = JWT.makeJWT(username)
-    res.send({message:"valid login",data:token});
+    var token = JWT.makeJWT(username);
+    res.send({ message: "valid login", data: token });
   } else {
-    res.send({message:"invalid login",data:'false'});
+    res.send({ message: "invalid login", data: "false" });
   }
 });
-app.post("/checkToken",function(req,res){
-  var token = req.body.token
+app.post("/checkToken", function (req, res) {
+  var token = req.body.token;
   //console.log(req.body.token)
-  if(token == null || token == 'false'){
-    res.send({message:"invalid token"})
-  }
-  else{
-
+  if (token == null || token == "false") {
+    res.send({ message: "invalid token" });
+  } else {
     try {
-      var data = JWT.verifyJWT(token)
+      var data = JWT.verifyJWT(token);
     } catch (error) {
-      res.send({message:"invalid token"})
+      res.send({ message: "invalid token" });
       return;
     }
-    if(config.username=data.data.username){
-      res.send({message:"valid token"})
-    }else{
-      res.send({message:"invalid token"})
-    } 
+    if ((config.username = data.data.username)) {
+      res.send({ message: "valid token" });
+    } else {
+      res.send({ message: "invalid token" });
+    }
   }
-})
+});
 
-app.post("/solidColor",function(req,res){
-    var token = req.body.token
-    var data = req.body.data
-    if(token != null && token != 'false'){
-      try {
-        var data = JWT.verifyJWT(token)
-      } catch (error) {
-        console.log('invalid token')
-        res.send({message:'invalid token'})
-        return;
-      }
-      if(config.username=data.data.username){
-        res.send({message:'valid token'})
-        //console.log('change color')
-        let python = spawn('python',['LED-Code/test.py',data[0],data[1],data[2]])
-        python.stdout.on('data',function(data){
-          pData = data.toString()
-          //console.log(pData)
-        })
-      }
-      else{
-        res.send({message:'invalid credentials'})
-      }
+app.post("/solidColor", function (req, res) {
+  var token = req.body.token;
+  var data = req.body.data;
+  if (token != null && token != "false") {
+    try {
+      var tokenData = JWT.verifyJWT(token);
+    } catch (error) {
+      console.log("invalid token");
+      res.send({ message: "invalid token" });
+      return;
     }
-    else{
-      res.send({message:'invalid token'})
+    if ((config.username = tokenData.data.username)) {
+      res.send({ message: "valid token" });
+      console.log('change color')
+      console.log(data)
+      let python = spawn("python", [
+        "LED-Code/test.py",
+        data[0],
+        data[1],
+        data[2],
+      ]);
+      python.stdout.on("data", function (data) {
+        pData = data.toString();
+        //console.log(pData)
+      });
+    } else {
+      res.send({ message: "invalid credentials" });
     }
-})
+  } else {
+    res.send({ message: "invalid token" });
+  }
+});
 
 server.listen(serverPort, () => {
   console.log("server up and running at %s port", serverPort);
