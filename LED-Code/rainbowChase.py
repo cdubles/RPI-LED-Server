79efef33e293b0bc828a08cc3f91fa29e8d37c5d
Cody <33189090+cdubles@users.noreply.github.com>
2023-03-26 16:04:10 -0400

    Update rainbowChase.py

--- LED-Code/rainbowChase.py
+++ LED-Code/rainbowChase.py
@@ -20,6 +20,6 @@ while True:
     for j in range(255):
             for i in range(pixelCount):
                 rc_index = (i * 256 // pixelCount) + j
-                pixels[i+20] =  wheel(rc_index & 255)
+                pixels[i+20+j] =  wheel(rc_index & 255)
                 print(f"{wheel(rc_index & 255)}: {i}")
             time.sleep(.5)
