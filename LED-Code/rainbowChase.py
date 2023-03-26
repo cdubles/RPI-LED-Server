c42bd8a541e99db86d5c0b86583f934ca7cc88cd
Cody <33189090+cdubles@users.noreply.github.com>
2023-03-26 16:01:21 -0400

    Update rainbowChase.py

--- LED-Code/rainbowChase.py
+++ LED-Code/rainbowChase.py
@@ -7,6 +7,7 @@ pixelCount  = 10
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
         return (0, 0, 0)
     if pos < 85:
         return (255 - pos * 3, pos * 3, 0)
