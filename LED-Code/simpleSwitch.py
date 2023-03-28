6354bf5f4152a2916c25d9d94032f1d0a6d19db4
Cody <33189090+cdubles@users.noreply.github.com>
2023-03-28 12:18:47 -0400

    Update simpleSwitch.py

--- LED-Code/simpleSwitch.py
+++ LED-Code/simpleSwitch.py
@@ -7,9 +7,11 @@ pixelCount  = 150
 offset = 0
 RED = (255,0,0)
 BLUE = (0,0,255)
+inc = 0
 while True:
     for i in range(pixelCount):
-        if i % 2 == 0:
+        if (i+inc) % 2 == 0:
             pixels[i] = RED
         else:
             pixels[i] = BLUE
+    inc+=1
